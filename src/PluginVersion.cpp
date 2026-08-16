401503a737a76b68b3f1c62ddb5daa1f0aa6c256
Apocrypher00 <coltonkenney@hotmail.com>
2026-08-15 18:34:41 -0700

    Add support for older Anniversary Edition runtimes

---
+++ src/PluginVersion.cpp
@@ -0,0 +1,20 @@
+#include "REL/Version.h"
+#include "SKSE/SKSE.h"
+
+SKSEPluginInfo(
+    .Version = REL::Version{ 1, 1, 0, 0 },
+    .Name = "WorldMapSelector"sv,
+    .Author = "Apocrypher00"sv,
+    .SupportEmail = ""sv,
+    .StructCompatibility = SKSE::StructCompatibility::Independent,
+    .RuntimeCompatibility = {
+        REL::Version{ 1, 6, 318, 0 },
+        REL::Version{ 1, 6, 323, 0 },
+        REL::Version{ 1, 6, 342, 0 },
+        REL::Version{ 1, 6, 353, 0 },
+        REL::Version{ 1, 6, 629, 0 },
+        REL::Version{ 1, 6, 640, 0 },
+        REL::Version{ 1, 6, 1130, 0 },
+        REL::Version{ 1, 6, 1170, 0 }
+    }
+)
